a0c207988217b098f06023c8606b75ea4064b3b8
SqkEMBD <amornsak-ra@starsmicro.com>
2023-06-22 10:50:42 +0700

    Added enumeration

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
  * @brief Import Arduino libraries.
  */
 #include <Arduino.h>
+#include <AccelStepper.h>
 
 /**
  * @brief Private macros
@@ -62,12 +63,62 @@ int lastButtonState = LOW; // the previous reading from the input pin
 unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
 unsigned long debounceDelay = 50;	// the debounce time; increase if the output flickers
 
-//# Fixed Paremeters for auto-injection mode.
+// # Fixed Paremeters for auto-injection mode.
 int time1 = 400;
 int time2 = 20;
 int speed1 = 1000;
 int speed2 = 5000;
 int pulseInterval = 400;
+
+AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin);
+
+// # Injection interval level
+enum
+{
+	INTERVAL_1 = 400,  // Injection interval 0.4 seconds
+	INTERVAL_2 = 600,  // Injection interval 0.6 seconds
+	INTERVAL_3 = 800,  // Injection interval 0.8 seconds
+	INTERVAL_4 = 1000, // Injection interval 1.0 seconds
+	INTERVAL_5 = 1200, // Injection interval 1.2 seconds
+	INTERVAL_6 = 1400, // Injection interval 1.4 seconds
+	INTERVAL_7 = 1600, // Injection interval 1.6 seconds
+	INTERVAL_8 = 1800, // Injection interval 1.8 seconds
+	INTERVAL_9 = 2000, // Injection interval 2.0 seconds
+};
+
+// # Injection speed level
+enum
+{
+	INJECT_SPEED_1 = 1, // Linear Actuator Speed 0.45 mm/s, 45 Hz
+	INJECT_SPEED_2 = 2, // Linear Actuator Speed 0.50 mm/s, 50 Hz
+	INJECT_SPEED_3 = 3, // Linear Actuator Speed 0.58 mm/s, 58 Hz
+	INJECT_SPEED_4 = 4, // Linear Actuator Speed 0.70 mm/s, 70 Hz
+	INJECT_SPEED_5 = 5, // Linear Actuator Speed 0.80 mm/s, 80 Hz
+	INJECT_SPEED_6 = 6, // Linear Actuator Speed 1.25 mm/s, 125 Hz
+	INJECT_SPEED_7 = 7, // Linear Actuator Speed 1.50 mm/s, 150 Hz
+	INJECT_SPEED_8 = 8, // Linear Actuator Speed 2.00 mm/s, 200 Hz
+	INJECT_SPEED_9 = 9, // Linear Actuator Speed 3.00 mm/s, 300 Hz
+};
+
+// # Leak prevention level
+enum 
+{
+	LEAK_PREVENT_1 = 1, // Plunger reverse time 100 ms
+	LEAK_PREVENT_2 = 2, // Plunger reverse time 160 ms
+	LEAK_PREVENT_3 = 3, // Plunger reverse time 230 ms
+	LEAK_PREVENT_4 = 4, // Plunger reverse time 340 ms
+	LEAK_PREVENT_5 = 5, // Plunger reverse time 440 ms
+	LEAK_PREVENT_6 = 6, // Plunger reverse time 550 ms
+};
+
+// # Vacuum level
+enum 
+{
+	VACUUM_THIN = 1, 	// Vacuum pressure 90~120 mmHg, 12~16 kPa
+	VACUUM_GENERAL = 2, // Vacuum pressure 140~190 mmHg, 19~25 kPa
+	VACUUM_THICK = 3,	// Vacuum pressure 210~300 mmHg, 28~40 kPa
+	VACUMM_MORE = 4		// Vacuum pressure 370~480 mmHg, 49~65 kPa
+};
 
 void setup()
 {
@@ -93,6 +144,9 @@ void setup()
 	// pinMode(POT_PIN, INPUT);
 
 	Serial.println("Program Starting");
+
+	stepper.setMaxSpeed(1000);
+	stepper.setSpeed(50);
 }
 
 void loop()
@@ -116,6 +170,7 @@ void loop()
 	{
 		nextLabState();
 	}
+	// stepper.runSpeed();
 }
 
 // Arduino Button Using Debouncing technique.
@@ -189,8 +244,9 @@ void programLab1(void)
 			digitalWrite(dirPin, HIGH);
 			// # Generation Pulse
 			digitalWrite(stepPin, HIGH);
-			delayMicroseconds(500);
+			delayMicroseconds(100);
 			digitalWrite(stepPin, LOW);
+			delayMicroseconds(100);
 		}
 		else if (digitalRead(ccwButton) == LOW)
 		{
@@ -200,9 +256,11 @@ void programLab1(void)
             digitalWrite(stepPin, HIGH);
             delayMicroseconds(500);
             digitalWrite(stepPin, LOW);
+			delayMicroseconds(500);
 		}
 		// # Delay for interval pulse signal.
-		delayMicroseconds(100);
+		// delay(1000);
+		// delayMicroseconds(100);
 	}
 	if (switchEvent)
 	{
